98166ea074c6c0dde668e523d544a5451580eed8
Kennedy Mota <kennedysantosmota22@gmail.com>
2021-07-18 16:52:13 -0300

    First commit adding Html and CSS to the project

---
+++ script.js
@@ -0,0 +1,15 @@
+var oneBtn = document.getElementById('calc-one')
+var twoBtn = document.getElementById('calc-two')
+var threeBtn = document.getElementById('calc-three')
+var fourBtn = document.getElementById('calc-four')
+var fiveBtn = document.getElementById('calc-five')
+var sixBtn = document.getElementById('calc-six')
+var sevenBtn = document.getElementById('calc-seven')
+var eightBtn = document.getElementById('calc-eight')
+var nineBtn = document.getElementById('calc-nine')
+var zeroBtn = document.getElementById('calc-zero')
+
+var display = document.getElementById('display')
+var clearBtn = document.getElementById('calc-clear')
+var pm = document.getElementById('calc-pm')
+
